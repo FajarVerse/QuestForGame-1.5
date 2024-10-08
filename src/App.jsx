080dc447ebb1b1140cd1a400f232e75f6fc7ff33
Gifarfajar321 <gifarifajarm174@gmail.com>
2024-10-08 19:11:08 +0700

    membuat hero section

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,6 @@ function App() {
     if (window.scrollY > fixNav) {
       HeaderRef.current.classList.add("header-transition");
       HeaderFixedRef.current.classList.add("header-fixed");
-
     } else {
       HeaderRef.current.classList.remove("header-transition");
       HeaderFixedRef.current.classLista.remove("header-fixed");
@@ -28,17 +27,20 @@ function App() {
     <>
       <header ref={HeaderFixedRef} className="absolute top-0 w-full h3">
         <div
-          className="w-full mx-auto px-3 py-2  transition duration-500 ease-in-out rounded-[0]"
+          className="w-full bg-transparent mx-auto px-5 py-4 transition duration-500 ease-in-out rounded-[0] lg:px-10"
           ref={HeaderRef}
         >
           <div className="container">
             <div className="flex items-center justify-between relative">
-              <div className="px-4">
-                <a href="" className="block font-bold text-xl lg:text-2xl">
+              <div className="px-1">
+                <a
+                  href=""
+                  className="block font-bold text-xl text-second md:text-2xl lg:text-3xl"
+                >
                   QuestForGame
                 </a>
               </div>
-              <div className="px-4 flex items-center relative lg:hidden">
+              <div className="flex items-center relative lg:hidden">
                 <button
                   id="hamburger"
                   name="hamburger"
@@ -59,7 +61,7 @@ function App() {
                   <li className="group">
                     <a
                       href="#"
-                      className="text-base text-slate-700 py-2 mx-8 flex lg:text-lg"
+                      className="text-base text-dark py-2 mx-8 flex lg:text-lg lg:text-white"
                     >
                       Home
                     </a>
@@ -67,7 +69,7 @@ function App() {
                   <li className="group">
                     <a
                       href="#"
-                      className="text-base text-slate-700 py-2 mx-8 flex lg:text-lg"
+                      className="text-base text-dark py-2 mx-8 flex lg:text-lg lg:text-white"
                     >
                       Games
                     </a>
@@ -75,7 +77,7 @@ function App() {
                   <li className="group">
                     <a
                       href="#"
-                      className="text-base text-slate-700 py-2 mx-8 flex lg:text-lg"
+                      className="text-base text-dark py-2 mx-8 flex lg:text-lg lg:text-white"
                     >
                       Movies
                     </a>
@@ -83,7 +85,7 @@ function App() {
                   <li className="group">
                     <a
                       href="#"
-                      className="text-base text-slate-700 py-2 mx-8 flex lg:text-lg"
+                      className="text-base text-dark py-2 mx-8 flex lg:text-lg lg:text-white"
                     >
                       News
                     </a>
@@ -94,8 +96,34 @@ function App() {
           </div>
         </div>
       </header>
-
-      <section className="min-h-[1000px]"></section>
+      {/* Hero Section */}
+      <section className="pt-40 pb-32 bg-dark min-h-screen">
+        <div className="container max-w-full">
+          <div className="">
+            <div className="w-full px-5 mx-auto">
+              <div className="text-center">
+                <h4 className="font-bold text-2xl text-primary mb-1 md:text-3xl lg:text-5xl">
+                  Welcome To
+                </h4>
+                <h2 className="font-extrabold text-4xl text-second mb-3 md:text-5xl lg:text-7xl">
+                  QuestForGame
+                </h2>
+                <p className="font-medium text-base text-description md:text-lg xl:text-xl">
+                  Jelajahi dunia games, dan temukan game impian mu
+                </p>
+              </div>
+              <div className="flex justify-center">
+                <a className="block font-medium text-lg text-second px-5 py-2 bg-primary m-5 rounded-full group hover:opacity-70 transition duration-300 ease-in-out md:px-7 md:text-xl xl:px-8">
+                  Start!!{" "}
+                  <span className="inline-block ml-0.5 rotate-[30deg] group-hover:-rotate-1 transition-all duration-300 ease-out">
+                    🚀
+                  </span>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#ff8906",
+        dark: "#0f0e17",
+        second: "#fffffe",
+        description: "#a7a9be;",
+      },
+    },
   },
   plugins: [],
 };
