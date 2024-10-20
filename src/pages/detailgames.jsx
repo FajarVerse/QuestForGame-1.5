a98faa4956f54e3b459bc9feb62d79e4cde6f6f2
Gifarfajar321 <gifarifajarm174@gmail.com>
2024-10-20 19:33:36 +0700

    membuat header dan layout screnshoot

--- src/pages/detailgames.jsx
+++ src/pages/detailgames.jsx
@@ -3,6 +3,8 @@ import Navbar from "../components/layouts/Navbar";
 import DetailGame from "../hooks/detailGame";
 import AuthLayout from "../components/layouts/AuthLayout";
 import Header from "../components/elements/Header";
+import CardLayout from "../components/layouts/CardLayout";
+import ToggleScrollX from "../components/fragments/ToggleScrollX";
 
 const DetailGames = () => {
   const { id } = useParams();
@@ -13,11 +15,21 @@ const DetailGames = () => {
   return (
     <>
       <Navbar />
-      <AuthLayout background="bg-dark" className="pt-32 pb-10">
-        <Header className="text-primary">Nama span</Header>
+      <AuthLayout background="bg-dark" className="pt-32 pb-16">
+        <div className="w-full px-5">
+          <Header className="text-primary">Nama Game</Header>
+        </div>
       </AuthLayout>
-      <AuthLayout background="bg-dark">
-        
+      <AuthLayout background="bg-dark" className="pt-10">
+        <div className="w-full px-7 lg:px-14">
+          <div className="relative w-full py-3.5 flex overflow-x-scroll gap-3 mb-2 scroll-smooth card-scroll-bar lg:gap-4">
+            <div className="w-4/5 h-44 bg-sunset box-shadow border-4 border-lightSunset flex-shrink-0 mr-2 md:w-3/5 md:h-56 lg:w-1/2 lg:h-64"></div>
+            <div className="w-4/5 h-44 bg-sunset box-shadow border-4 border-lightSunset flex-shrink-0 mr-2 md:w-3/5 md:h-56 lg:w-1/2 lg:h-64"></div>
+            <div className="w-4/5 h-44 bg-sunset box-shadow border-4 border-lightSunset flex-shrink-0 mr-2 md:w-3/5 md:h-56 lg:w-1/2 lg:h-64"></div>
+            <div className="w-4/5 h-44 bg-sunset box-shadow border-4 border-lightSunset flex-shrink-0 mr-2 md:w-3/5 md:h-56 lg:w-1/2 lg:h-64"></div>
+          </div>
+          <ToggleScrollX />
+        </div>
       </AuthLayout>
     </>
   );
